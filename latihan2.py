1e48b80d9c5808de5073dad26b2037138ad8b70e
mrbaihaqi <108067634+mrbaihaqi@users.noreply.github.com>
2023-01-29 19:20:13 +0700

    Create latihan2.py

---
+++ latihan2.py
@@ -0,0 +1,14 @@
+print("---Latihan 2----")
+print("menampilkan bilangan berhenti ketika bilangan 0 dan menampilkan bilangan terbesar")
+
+max=0
+while True:
+    a=int(input("masukan bilangan : "))
+    if max < a :
+        max = a
+    if a==0:
+        break
+print("bilangan terbesar adalah = ",max)
+#sourcode by
+#mrbaihaqi
+#312210720
